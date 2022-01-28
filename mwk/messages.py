0e4f946a67f15b4a9cd053fb7e085c5b7a595a62
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-01-28 12:27:28 +0530

    Update messages.py

--- mwk/messages.py
+++ mwk/messages.py
@@ -1,14 +1,14 @@
 class Translation(object):
-    START_TEXT = """**Hello,
-This is a Telegram Rename Bot by @mcubemedia 😇
+    START_TEXT = """**Hello dear {} ,
+This is a File Rename Bot by @Telecat_X 😇
 
 Please send me any File, i can Rename It As Per Your choices to telegram as File/Video
 
 Hit /help for more details.. and more thing waiting for u there 😉**"""
 ######################
-    HELP_USER = """**1 >>Send File/Video\n2 >>Choose Your desired Option\n3 >>Wait for it to process files**"""
-    DOWNLOAD_MSG = "**Yup Bro, Downloading....**😌"
+    HELP_USER = """**Help Module**\n\n**1 >>Send File/Video\n2 >>Choose Your desired Option\n3 >>Wait for it to process files**"""
+    DOWNLOAD_MSG = "**Hurray.. I am Downloading**😌"
     DOWNLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
-    UPLOAD_MSG = "**Yeah Boy, Uploading it**🤓"
+    UPLOAD_MSG = "**Yeah.., Uploading please it**🤓"
     UPLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
     UPLOAD_DONE_MSG = "**Finally Uploaded Join My Update Channel.. if you like me 🤪"
