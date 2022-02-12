dd5736f440e92115075039bd5e53617851aab971
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-02-12 23:30:31 +0530

    Update messages.py

--- mwk/messages.py
+++ mwk/messages.py
@@ -1,7 +1,6 @@
 
-
 class Translation(object):
-    START_TEXT = """**Hello dear {message.from_user} ,
+    START_TEXT = """**Hello dear {message.from_user.mention()} ,
 This is a File Rename Bot by @Telecat_X 😇
 
 Please send me any File, i can Rename It As Per Your choices to telegram as File/Video
@@ -9,7 +8,7 @@ Please send me any File, i can Rename It As Per Your choices to telegram as File
 Hit /help for more details.. and more thing waiting for u there 😉**"""
 ######################
     HELP_USER = """**Help Module**\n\n**1 >>Send File/Video\n2 >>Choose Your desired Option\n3 >>Wait for it to process files**"""
-    DOWNLOAD_MSG = "**Hurray.. I am Downloading**😌"
+    DOWNLOAD_MSG = "**Please wait.. I am Downloading**😌"
     DOWNLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
     UPLOAD_MSG = "**Yeah.., Uploading please wait it**🤓"
     UPLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
